6d008d08b1dec75f4ff4add3965dc86df59c03e3
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-06-24 15:37:36 +0100

    Get project thumbnail to fade on hover

--- components/project-thumbnail.js
+++ components/project-thumbnail.js
@@ -1,8 +1,45 @@
-import styles from './project.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import Icon from '../components/tech-icon'
-import Image from 'next/image'
+import styles from "./project.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import Icon from "./tech-icon";
+import Image from "next/image";
+import React, { useState } from "react";
+
+export default function ProjectThumbnail(props) {
+  const [fadedState, setFadedState] = useState(false);
+  const [invisibleState, setInvisibleState] = useState(true);
+
+  return (
+    <div
+      className={utilStyles.relative}
+      onMouseEnter={() => {
+        setFadedState(true);
+        setInvisibleState(false);
+      }}
+      onMouseLeave={() => {
+        setFadedState(false);
+        setInvisibleState(true);
+      }}>
+      <Image
+        className={fadedState ? utilStyles.faded : ""}
+        src={props.thumbnail}
+        height={300}
+        width={300}
+        alt={props.name}
+      />
+      {/* <div> */}
+      <ul className={invisibleState ? utilStyles.invisible : styles.overlay}>
+        {props.techstack.map((tech) => (
+          <Icon
+            className={styles.stack_card}
+            name={`${tech}`}
+            id={`${tech}-icon`}></Icon>
+        ))}
+      </ul>
+      {/* </div> */}
+    </div>
+  );
+}
 
 // export default function Project(props) {
 //     return (
@@ -46,17 +83,3 @@ import Image from 'next/image'
 //       </article>
 //   )
 // }
-
-export default function ProjectThumbnail(props) {
-  return (
-      <>
-      <Image
-          src={props.thumbnail}
-          // className={utilStyles.borderCircle}
-          height={300}
-          width={300}
-          alt={props.name}
-      />
-      </>
-  )
-}
--- pages/my-work.js
+++ pages/my-work.js
@@ -1,42 +1,47 @@
-import Layout, { siteTitle } from '../components/layout'
-import ProjectThumbnail from '../components/project'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedData, projectsDirectory } from '../lib/markdown-files'
-import Link from 'next/link'
-import Date from '../components/date'
-import Image from 'next/image'
+import Layout, { siteTitle } from "../components/layout";
+import ProjectThumbnail from "../components/project-thumbnail";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedData, projectsDirectory } from "../lib/markdown-files";
+import Link from "next/link";
+import Date from "../components/date";
+import Image from "next/image";
 
 export async function getStaticProps() {
-  const allProjectsData = getSortedData(projectsDirectory)
+  const allProjectsData = getSortedData(projectsDirectory);
   return {
     props: {
-      allProjectsData
-    }
-  }
+      allProjectsData,
+    },
+  };
 }
 
 export default function Work({ allProjectsData }) {
-   return (
-      <Layout>
-           <h1>My Work</h1>
-           {/* <Project project_name={"TeleGran"}></Project>
+  return (
+    <Layout>
+      <h1>My Work</h1>
+      {/* <Project project_name={"TeleGran"}></Project>
          <Project project_name={"Grinder"}></Project> */}
-         ---
-         
-            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-               <h2 className={utilStyles.headingLg}>Projects</h2>
-                  <ul className={`${utilStyles.list} ${utilStyles.grid}`}>
-               {/* thumbnail of picture with overlay of  tech stack icons */}
-               {allProjectsData.map(({ id, techstack, name, thumbnail }) => (
-                  
-                     <li className={utilStyles.listItem} key={id}>
-                        <Link href={`/projects/${id}`}>
-                           <a>{<ProjectThumbnail thumbnail={thumbnail} name={name}></ProjectThumbnail>}</a>
-                        </Link>
-                     </li>
-                  ))}
-               </ul>
-         </section>
-      </Layout>	  
-   )
+      ---
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Projects</h2>
+        <ul className={`${utilStyles.list} ${utilStyles.grid}`}>
+          {/* thumbnail of picture with overlay of  tech stack icons */}
+          {allProjectsData.map(({ id, techstack, name, thumbnail }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/projects/${id}`}>
+                <a>
+                  {
+                    <ProjectThumbnail
+                      thumbnail={thumbnail}
+                      name={name}
+                      techstack={techstack}></ProjectThumbnail>
+                  }
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
 }
